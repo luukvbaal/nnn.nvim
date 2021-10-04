0532173e23a1d64754632c17b422264221fd130b
Luuk van Baal <luukvbaal@gmail.com>
2021-10-04 22:16:44 +0200

    feat: allow custom function mappings

--- lua/nnn.lua
+++ lua/nnn.lua
@@ -1,23 +1,27 @@
 local api = vim.api
 local uv = vim.loop
 local cmd = vim.cmd
+local fn = vim.fn
 local pickerregex = "term://.*nnn.*-p.*";
 local fiforegex = "term://.*nnn.*-F1";
+local regex = fiforegex
 local curwin
-local pickertmp = vim.fn.tempname() .. "-picker"
-local fifotmp = vim.fn.tempname() .. "-explorer"
+local action
+local pickertmp = fn.tempname() .. "-picker"
+local fifotmp = fn.tempname() .. "-explorer"
 local opts = os.getenv("NNN_OPTS"):gsub("a", "")
 local cfg = {
-	cmd = "nnn",
+	explorercmd = "nnn",
+	pickercmd = "nnn",
 	replace_netrw = false,
 	filetype_exclude = {},
 	default_mode = "explorer",
 	explorer_width = 24,
-	mappings = {}
+	mappings = {},
 }
 local M = {}
 
-local function is_buf_open(regex)
+local function get_buf()
  	for _, buf in pairs(api.nvim_list_bufs()) do
 		local buf_name = api.nvim_buf_get_name(buf)
 		if string.match(buf_name, regex) ~= nil then return buf end
@@ -25,25 +29,30 @@ local function is_buf_open(regex)
 	return nil
 end
 
-local function is_win_open(regex)
-	for _, win in pairs(api.nvim_list_wins()) do
- 		local buf = api.nvim_win_get_buf(win)
-		local buf_name = api.nvim_buf_get_name(buf)
-		if string.match(buf_name, regex) ~= nil then return buf end
-  end
-    return nil
-end
-
-local function get_win(regex)
- 	for _, win in pairs(api.nvim_list_wins()) do
+local function get_win()
+ 	for _, win in pairs(api.nvim_tabpage_list_wins(api.nvim_tabpage_get_number(0))) do
 		local buf_name = api.nvim_buf_get_name(api.nvim_win_get_buf(win))
 		if string.match(buf_name, regex) ~= nil then return win end
 	end
   return nil
 end
 
-function M.close(regex)
-  local win = get_win(regex)
+local function filter_curwin_nnn()
+	local windows = api.nvim_list_wins()
+	curwin = api.nvim_tabpage_get_win(api.nvim_tabpage_get_number(0))
+	regex = (regex == fiforegex) and pickerregex or fiforegex
+	if get_win() == curwin then
+		if #windows == 1 then
+			cmd("vsplit")
+		else
+			curwin = windows[2]
+		end
+	end
+	regex = (regex == pickerregex) and fiforegex or pickerregex
+end
+
+local function close()
+  local win = get_win()
   if not win then return end
 	if #api.nvim_list_wins() == 1 then
 		api.nvim_win_set_buf(win, api.nvim_create_buf(false, false))
@@ -68,7 +77,7 @@ local function read_fifo()
 							local win = get_win()
 							local portwidth = api.nvim_win_get_width(win)
 							local width = portwidth - cfg.explorer_width
-							cmd(width .. "vsplit " .. vim.fn.fnameescape(chunk:sub(1, -2)))
+							cmd(width .. "vsplit " .. fn.fnameescape(chunk:sub(1, -2)))
 							curwin = api.nvim_tabpage_get_win(0)
 							api.nvim_set_current_win(win)
 							-- TODO replace workaround for nnn shifting out of viewport
@@ -77,7 +86,7 @@ local function read_fifo()
 							api.nvim_feedkeys(api.nvim_replace_termcodes("<C-\\><C-n><C-W>l", true, true, true), "t", true)
 						else
 							api.nvim_set_current_win(curwin)
-							cmd("edit " .. vim.fn.fnameescape(chunk:sub(1, -2)))
+							cmd("edit " .. fn.fnameescape(chunk:sub(1, -2)))
 						end
 					end, 0)
 				else
@@ -88,23 +97,16 @@ local function read_fifo()
 	end)
 end
 
-function M.mapping(action)
-	api.nvim_feedkeys(api.nvim_replace_termcodes("<C-\\><C-n>", true, true, true), "t", true)
-	api.nvim_set_current_win(curwin)
-	vim.cmd(action)
-	api.nvim_set_current_win(get_win())
-	api.nvim_feedkeys(api.nvim_replace_termcodes("i<CR>", true, true, true), "t", true)
-end
-
-function M.open(regex)
-	curwin = api.nvim_tabpage_get_win(0)
-	local buf = is_buf_open(regex)
+local function open_explorer()
+	if get_win() then return end
+	filter_curwin_nnn()
+	local buf = get_buf()
 	if buf == nil then
-		cmd("topleft" .. cfg.explorer_width .. "vsplit term://NNN_OPTS=" .. opts .. " NNN_FIFO=" .. fifotmp .. " " .. cfg.cmd .. " -F1")
+		cmd("topleft" .. cfg.explorer_width .. "vsplit term://NNN_OPTS=" .. opts .. " NNN_FIFO=" .. fifotmp .. " " .. cfg.explorercmd .. " -F1")
 		cmd("setlocal nonumber norelativenumber winfixwidth winfixheight noshowmode buftype=terminal filetype=nnn")
-		api.nvim_buf_set_keymap(is_buf_open(regex), "t", "<C-l>", "<C-\\><C-n><C-w>l", {})
-		for k, v in pairs(cfg.mappings) do
-			api.nvim_buf_set_keymap(is_buf_open(regex), "t", k, "<C-\\><C-n><cmd>lua require('nnn').mapping('" .. v .."')<CR>", {})
+		api.nvim_buf_set_keymap(get_buf(), "t", "<C-l>", "<C-\\><C-n><C-w>l", {})
+		for i = 1, #cfg.mappings do
+			api.nvim_buf_set_keymap(get_buf(), "t", cfg.mappings[i][1], "<C-\\><C-n><cmd>lua require('nnn').mapping('" .. i .. "')<CR>", {})
 		end
 		read_fifo()
 	else
@@ -114,15 +116,14 @@ function M.open(regex)
 end
 
 local function create_float()
-	curwin = api.nvim_tabpage_get_win(0)
-  local vim_height = vim.api.nvim_eval [[&lines]]
-  local vim_width = vim.api.nvim_eval [[&columns]]
+  local vim_height = api.nvim_eval("&lines")
+  local vim_width = api.nvim_eval("&columns")
 
   local width = math.floor(vim_width * 0.8) + 5
   local height = math.floor(vim_height * 0.7) + 2
   local col = vim_width * 0.1 - 2
   local row = vim_height * 0.15 - 1
-  local win = vim.api.nvim_open_win(0, true, {
+  local win = api.nvim_open_win(0, true, {
       relative = 'editor',
       width = width,
       height = height,
@@ -131,37 +132,41 @@ local function create_float()
       style = 'minimal',
       focusable = false
     })
-	if #api.nvim_list_bufs() == 1 or is_buf_open(pickerregex) == nil then
+	if #api.nvim_list_bufs() == 1 or get_buf() == nil then
 		local buf = api.nvim_create_buf(true, false)
 		cmd("keepalt b" .. buf)
 	end
 	return win
 end
 
-function M.on_exit()
-	M.close(pickerregex)
+local function on_exit()
+	close()
 	local fd = io.open(pickertmp, "r")
 	if fd ~= nil then
-		for line in io.lines(pickertmp) do
-			cmd("edit " .. vim.fn.fnameescape(line))
+		if action ~= nil then
+			action(io.lines(pickertmp))
+		else
+			for line in io.lines(pickertmp) do
+				cmd("edit " .. fn.fnameescape(line))
+			end
 		end
 	else
 		print("error exiting nnn")
 	end
 	io.close(fd)
+	action = nil
 end
 
-local function open_picker(regex)
+local function open_picker()
+	filter_curwin_nnn()
 	local win = create_float()
-	curwin = api.nvim_tabpage_get_win(0)
-	local buf = is_buf_open(regex)
+	local buf = get_buf()
 	if buf == nil then
-		vim.fn.termopen("nnn -p " .. pickertmp, { on_exit = M.on_exit })
-		--cmd("edit term://NNN_OPTS=" .. opts .. " " .. cfg.cmd .. " -p" .. fname)
+		fn.termopen(cfg.pickercmd .. " -p " .. pickertmp, { on_exit = on_exit })
 		cmd("setlocal nonumber norelativenumber winfixwidth winfixheight noshowmode buftype=terminal filetype=nnn")
-		api.nvim_buf_set_keymap(is_buf_open(regex), "t", "<C-l>", "<C-\\><C-n><C-w>l", {})
-		for k, v in pairs(cfg.mappings) do
-			api.nvim_buf_set_keymap(is_buf_open(regex), "t", k, "<C-\\><C-n><cmd>lua require('nnn').mapping('" .. v .."')<CR>", {})
+		api.nvim_buf_set_keymap(get_buf(), "t", "<C-l>", "<C-\\><C-n><C-w>l", {})
+		for i = 1, #cfg.mappings do
+			api.nvim_buf_set_keymap(get_buf(), "t", cfg.mappings[i][1], "<C-\\><C-n><cmd>lua require('nnn').mapping('" .. i .. "')<CR>", {})
 		end
 	else
 		api.nvim_win_set_buf(win, buf)
@@ -172,26 +177,41 @@ end
 function M.toggle(mode)
 	if mode == nil then mode = cfg.default_mode end
 	if mode == "explorer" then
-		if is_win_open(fiforegex) then
-			M.close(fiforegex)
+		regex = fiforegex
+		if get_win() then
+			close()
 		else
-			M.open(fiforegex)
+			open_explorer()
   	end
 	elseif mode == "picker" then
-		if is_win_open(pickerregex) then
-			M.close(pickerregex)
+		regex = pickerregex
+		if get_win() then
+			close()
 		else
-			open_picker(pickerregex)
+			open_picker()
 		end
 	end
+end
+
+function M.mapping(map)
+	api.nvim_feedkeys(api.nvim_replace_termcodes("<C-\\><C-n>", true, true, true), "t", true)
+	api.nvim_set_current_win(curwin)
+	local mapping = cfg.mappings[tonumber(map)][2]
+	if type(mapping) == "function" then
+		action = mapping
+	else
+		cmd(mapping)
+	end
+	if get_win() == nil then open_explorer() end
+	api.nvim_set_current_win(get_win())
+	api.nvim_feedkeys(api.nvim_replace_termcodes("i<CR>", true, true, true), "t", true)
 end
 
 function M.setup(setup_cfg)
 	cmd [[
 		command! NnnPicker lua require("nnn").toggle("picker")
+		command! NnnExplorer lua require("nnn").toggle("explorer")
 		command! NnnToggle lua require("nnn").toggle()
-		command! NnnOpen lua require("nnn").open()
-		command! NnnClose lua require("nnn").close()
 		autocmd BufEnter * if &ft ==# "nnn" | startinsert | endif
 		autocmd TermClose * if &ft ==# "nnn" | :bdelete! | endif
 	]]
