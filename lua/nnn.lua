a2fbeb383c3ef592fa81dff1a2abe486bdf370eb
Luuk van Baal <luukvbaal@gmail.com>
2021-11-23 18:15:47 +0100

    fix: clear netrw autocmd before schedule fix #26

--- lua/nnn.lua
+++ lua/nnn.lua
@@ -424,22 +424,15 @@ function M.setup(setup_cfg)
 
 	-- Replace netrw plugin if config is set
 	if cfg.replace_netrw then
-		if not vim.g.loaded_netrw then
-			vim.g.loaded_netrw = 1
-			vim.g.loaded_netrwPlugin = 1
-			vim.g.loaded_netrwSettings = 1
-			vim.g.loaded_netrwFileHandles = 1
-		end
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+		vim.g.loaded_netrwSettings = 1
+		vim.g.loaded_netrwFileHandlers = 1
+		cmd("silent! autocmd! FileExplorer *")
 
 		schedule(function()
 			M.toggle(cfg.replace_netrw, nil, "netrw")
-
-			if api.nvim_buf_get_option(0, "filetype") == "netrw" then
-				api.nvim_buf_delete(0, {})
-			end
-
-			cmd([[silent! autocmd! FileExplorer *
-				autocmd BufEnter,BufNewFile * lua require('nnn').toggle(']]..cfg.replace_netrw..[[', nil, "netrw")]])
+			cmd("autocmd BufEnter,BufNewFile * lua require('nnn').toggle('"..cfg.replace_netrw.."', nil, 'netrw')")
 		end)
 	end
 
